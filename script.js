6ee5dae104443f35f326cacac278fda7e484c7a2
eivind-glodedata <eivind.glodedata@gmail.com>
2025-06-20 14:13:18 +0200

    fredags jobbing

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 console.log("Hallajen");
+
+async function fetchBS() {
+    //const fetchData = await fetch("https://foodish-api.com/");
+    //const fetchData = await fetch("https://www.fruityvice.com/");
+
+    // using corporate bullshit generator API
+    const fetchData = await fetch("https://corporatebs-generator.sameerkumar.website/");
+    
+    console.log(fetchData);
+
+    const data = await fetchData.json();
+    console.log(data);
+    console.log(data.phrase);
+
+    return data;
+};
+
+fetchBS();
+
+async function displayBS() {
+  //  const response = await fetchBS();
+   // console.log(response + "is the response");
+
+   // const corporateBs = response;
+   // console.log(corporateBs.phrase + "is this an array???");
+
+   // const factsList = document.getElementById("factsList");
+  //  const listItem = document.createElement("li");
+   // factsList.appendChild(listItem);
+  //  listItem.textContent = corporateBs.phrase;
+ // Array.from(corporateBs).forEach(element => {
+ //   forEach(myFunction())
+ // });
+ // function myFunction() {
+   // factsList.appendChild(listItem);
+  //  listItem.textContent = corporateBs.phrase; }
+
+  for (let i = 0; i < 100; i++) {
+    const response = await fetchBS();
+    const factsList = document.getElementById("factsList");
+    const listItem = document.createElement("li");
+    factsList.appendChild(listItem);
+    listItem.textContent = response.phrase;
+  };
+};
+
+displayBS();
